1de282ee31d13b8351e802e15d57bf2f9c69f602
zzj <598093760@qq.com>
2022-04-01 18:24:58 +0800

    登录模块

--- login/src/main/java/com/zzj/simpleeducationsysetm/LoginApplication.java
+++ login/src/main/java/com/zzj/simpleeducationsysetm/LoginApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@MapperScan("com.zzj.simpleeducationsysetm.Mappers")
+@MapperScan("com.zzj.simpleeducationsysetm.mappers")
 @SpringBootApplication
 public class LoginApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
     }
-
 }
