98b46cc3e009e02aa3b73b54d04e274da925507e
zzj <598093760@qq.com>
2022-04-01 20:03:47 +0800

    配置eureka client

--- login/src/main/java/com/zzj/login/LoginApplication.java
+++ login/src/main/java/com/zzj/login/LoginApplication.java
@@ -3,11 +3,16 @@ package com.zzj.login;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@MapperScan("com.zzj.simpleeducationsysetm.mappers")
+
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
+@MapperScan("com.zzj.login.mappers")
 public class LoginApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
