24ea16004d763c339f510221909e8fd88c19ed35
zzj <598093760@qq.com>
2022-04-02 14:48:50 +0800

    路由配好了

--- login/src/main/java/com/zzj/login/LoginApplication.java
+++ login/src/main/java/com/zzj/login/LoginApplication.java
@@ -11,7 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 @MapperScan("com.zzj.login.mappers")
 public class LoginApplication {
     public static void main(String[] args) {
