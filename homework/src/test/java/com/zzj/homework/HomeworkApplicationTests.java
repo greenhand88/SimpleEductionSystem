b1b8607052c7491daed65daa236258a3420bddf1
zzj <598093760@qq.com>
2022-04-02 14:50:45 +0800

    路由配好了

---
+++ homework/src/test/java/com/zzj/homework/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzj.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
